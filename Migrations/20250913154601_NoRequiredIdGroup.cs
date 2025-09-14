6e5c5e448df61f3ce194364fd844ea4772b2bab5
Xilobone <xilobone@gmail.com>
2025-09-14 18:17:00 +0200

    db id

---
+++ Migrations/20250913154601_NoRequiredIdGroup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace logbook.Migrations
+{
+    /// <inheritdoc />
+    public partial class NoRequiredIdGroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

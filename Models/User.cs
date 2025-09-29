f0ff28bfadbaa11e5f5c2096275459c45d28cf70
Xilobone <xilobone@gmail.com>
2025-09-29 22:52:02 +0200

    comments

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,33 @@
 namespace Logbook.Models
-{
+{   
+    /// <summary>
+    /// Represents a user of the program
+    /// </summary>
     public class User
-    {
+    {   
+        /// <summary>
+        /// The unique identifier of the user
+        /// </summary>
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// The display name of the user
+        /// </summary>
         public string UserName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The entra id corresponding to this user
+        /// </summary>
         public Guid EntraId { get; set; } = Guid.Empty;
+
+        /// <summary>
+        /// The id of the group the user belongs to
+        /// </summary>
         public Guid GroupId { get; set; } = Guid.Empty;
+
+        /// <summary>
+        /// The group the user belongs to
+        /// </summary>
         public Group? Group { get; set; }
     }
 }

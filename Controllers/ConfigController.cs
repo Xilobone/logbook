d9cdb66727c5dd10069680699aae62014e26dd71
Xilobone <xilobone@gmail.com>
2025-10-22 21:23:25 +0200

    get personal config

--- Controllers/ConfigController.cs
+++ Controllers/ConfigController.cs
@@ -27,12 +27,11 @@ namespace Logbook.Controllers
             _context = context;
         }
         /// <summary>
-        /// Gets the users personal configuration, as well as the config of any group they are part
-        /// of
+        /// Gets the users groups configuration
         /// </summary>
-        /// <returns>The configuration</returns>
-        [HttpGet]
-        public async Task<IActionResult> GetConfig()
+        /// <returns>The configuration of the groups the user is part of</returns>
+        [HttpGet("groups")]
+        public async Task<IActionResult> GetGroupsConfig()
         {
             string? incomingToken = await HttpContext.GetTokenAsync("access_token");
 
@@ -61,15 +60,48 @@ namespace Logbook.Controllers
         }
 
         /// <summary>
+        /// Gets the users personal configuration
+        /// </summary>
+        /// <returns>The users personal configuration</returns>
+        [HttpGet("me")]
+        public async Task<IActionResult> GetMeConfig()
+        {
+            string? incomingToken = await HttpContext.GetTokenAsync("access_token");
+
+            if (incomingToken == null) return Unauthorized("No token provided");
+
+            User? user = await Util.User.GetOrCreate(HttpContext, _context);
+
+            if (user == null)
+            {
+                Logger.Log("User is null");
+                return Problem("User doesnt exist");
+            }
+
+            var data = new
+            {
+                Id = user.EntraId,
+                user.CalendarName,
+                user.Enabled
+            };
+
+            return Ok(data);
+
+        }
+
+        /// <summary>
         /// Updates the configuration of a specified group
         /// </summary>
         /// <param name="param">The parameters used to update</param>
         /// <returns>A 200 code if the status was updated</returns>
-        [HttpPost("update")]
-        public IActionResult UpdateConfig([FromBody] UpdateConfigParams param)
+        [HttpPost("group/update")]
+        public IActionResult UpdateGroupConfig([FromBody] UpdateGroupConfigParams param)
         {
+            //TODO: add validation that user is actually in the group that it requested to update
+            Logger.Log("Update config endpoint called");
             if (!ModelState.IsValid)
             {
+                Logger.Log("Invalid parameters");
                 return BadRequest(ModelState);
             }
 
@@ -77,6 +109,8 @@ namespace Logbook.Controllers
 
             if (group == null) return NotFound($"No group with id {param.Id} was found");
 
+            Logger.Log($"group with id {param.Id} was found, named {group.Name}");
+            Logger.Log($"New name: {param.Name}");
             //update values
             if (!string.IsNullOrEmpty(param.Name)) group.Name = param.Name;
             if (!string.IsNullOrEmpty(param.FilePath)) group.FilePath = param.FilePath;
@@ -90,9 +124,39 @@ namespace Logbook.Controllers
         }
 
         /// <summary>
-        /// Parameters used for the update endpoint
+        /// Updates the configuration of a specified group
         /// </summary>
-        public class UpdateConfigParams
+        /// <param name="param">The parameters used to update</param>
+        /// <returns>A 200 code if the status was updated</returns>
+        [HttpPost("me/update")]
+        public async Task<IActionResult> UpdateMeConfig([FromBody] UpdateMeConfigParams param)
+        {   
+            Logger.Log("Update config endpoint called");
+            if (!ModelState.IsValid)
+            {
+                Logger.Log("Invalid parameters");
+                return BadRequest(ModelState);
+            }
+
+            string? incomingToken = await HttpContext.GetTokenAsync("access_token");
+            if (incomingToken == null) return Unauthorized("No token provided");
+
+            User? user = await Util.User.Get(HttpContext, _context);
+
+            if (user == null) return NotFound("User not registered");
+
+            if (!string.IsNullOrEmpty(param.CalendarName)) user.CalendarName = param.CalendarName;
+            if (param.Enabled != null) user.Enabled = (bool) param.Enabled;
+
+            _context.SaveChanges();
+
+            return Ok("data updated");
+        }
+
+        /// <summary>
+        /// Parameters used for the update group endpoint
+        /// </summary>
+        public class UpdateGroupConfigParams
         {
             /// <summary>
             /// The id of the group that is updated
@@ -124,6 +188,22 @@ namespace Logbook.Controllers
             /// The updated timezone
             /// </summary>
             public string? TimeZone { get; set; }
+        }
+
+        /// <summary>
+        /// Parameters used for the update me endpoint
+        /// </summary>
+        public class UpdateMeConfigParams
+        {
+            /// <summary>
+            /// The updated calendar name
+            /// </summary>
+            public string? CalendarName { get; set; }
+
+            /// <summary>
+            /// The updated enabled status
+            /// </summary>
+            public bool? Enabled { get; set; }
         }
     }
 }
--- UserUtil.cs
+++ UserUtil.cs
@@ -2,12 +2,12 @@ using Logbook.Data;
 using Graph = Microsoft.Graph.Models;
 using Microsoft.AspNetCore.Authentication;
 namespace Logbook.Util
-{   
+{
     /// <summary>
     /// Class containing util functions regarding users
     /// </summary>
     public class User
-    {   
+    {
         /// <summary>
         /// Gets the stored user info that made the request, or gets the info from graph and inserts
         /// it into the database if it doesnt exist yet
@@ -47,7 +47,24 @@ namespace Logbook.Util
             }
 
             return user;
+        }
 
+        /// <summary>
+        /// Gets the stored user info that made the request, or null if the user doesnt exist
+        /// </summary>
+        /// <param name="httpContext">The http context of the request</param>
+        /// <param name="_context">The database context</param>
+        /// <returns>The user that made the request</returns>
+        public static async Task<Models.User?> Get(HttpContext httpContext, LogbookDBContext _context)
+        {
+            var incomingToken = await httpContext.GetTokenAsync("access_token");
+            var graphClient = GraphClient.GetByAccessCode(incomingToken);
+
+            Guid entraId = GraphClient.GetUserEntraId(incomingToken!);
+
+            Models.User? user = _context.Users.Where(user => user.EntraId == entraId).FirstOrDefault();
+
+            return user;
         }
     }
 }

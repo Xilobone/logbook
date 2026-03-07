0318469e07923464bb22c449b05bcf885fc55b07
Xilobone <xilobone@gmail.com>
2026-03-07 18:11:21 +0100

    parameterized macros

--- Macros.cs
+++ Macros.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Logbook.Models;
 
 namespace Logbook
@@ -10,28 +11,25 @@ namespace Logbook
         const string TIME_FORMAT = "HH:mm:ss";
         const string DATE_FORMAT = "dd/MM/yyyy";
         const string DATETIME_FORMAT = "dd/MM/yyyy - HH:mm:ss";
-        static readonly IReadOnlyDictionary<string, Func<Event, Group, string>> macros =
-        new Dictionary<string, Func<Event, Group, string>>
+
+        static readonly IReadOnlyDictionary<string, Func<Event, Models.Group, string[], string>> parameterizedMacros =
+ new Dictionary<string, Func<Event, Models.Group, string[], string>>
+ {
+     ["EVENT.DATE"] = (e, g, p) => ConvertDateTime(e, g, DATE_FORMAT, p),
+     ["EVENT.DATETIME"] = (e, g, p) => ConvertDateTime(e, g, DATETIME_FORMAT, p),
+     ["EVENT.TIME"] = (e, g, p) => ConvertDateTime(e, g, TIME_FORMAT, p),
+ };
+
+        static readonly IReadOnlyDictionary<string, Func<Event, Models.Group, string>> macros =
+        new Dictionary<string, Func<Event, Models.Group, string>>
         {
             ["EVENT.TITLE"] = (e, g) => e.Title,
             ["EVENT.NOTES"] = (e, g) => e.Notes,
             ["EVENT.ORGANIZER"] = (e, g) => e.Organizer,
-            ["EVENT.DATE.START"] = (e, g) => ConvertDateTime(e.StartTime, g.TimeZone, DATE_FORMAT),
-            ["EVENT.DATE.START.UTC"] = (e, g) => ConvertDateTime(e.StartTime, "UTC", DATE_FORMAT),
-            ["EVENT.DATE.END"] = (e, g) => ConvertDateTime(e.EndTime, g.TimeZone, DATE_FORMAT),
-            ["EVENT.DATE.END.UTC"] = (e, g) => ConvertDateTime(e.EndTime, "UTC", DATE_FORMAT),
-            ["EVENT.DATETIME.START"] = (e, g) => ConvertDateTime(e.StartTime, g.TimeZone, DATETIME_FORMAT),
-            ["EVENT.DATETIME.START.UTC"] = (e, g) => ConvertDateTime(e.StartTime, "UTC", DATETIME_FORMAT),
-            ["EVENT.DATETIME.END"] = (e, g) => ConvertDateTime(e.EndTime, g.TimeZone, DATETIME_FORMAT),
-            ["EVENT.DATETIME.END.UTC"] = (e, g) => ConvertDateTime(e.EndTime, "UTC", DATETIME_FORMAT),
-            ["EVENT.TIME.START"] = (e, g) => ConvertDateTime(e.StartTime, g.TimeZone, TIME_FORMAT),
-            ["EVENT.TIME.START.UTC"] = (e, g) => ConvertDateTime(e.StartTime, "UTC", TIME_FORMAT),
-            ["EVENT.TIME.END"] = (e, g) => ConvertDateTime(e.EndTime, g.TimeZone, TIME_FORMAT),
-            ["EVENT.TIME.END.UTC"] = (e, g) => ConvertDateTime(e.EndTime, "UTC", TIME_FORMAT),
             ["GROUP.NAME"] = (e, g) => g.Name,
-            ["EVENT.ATTENDEES.ATTENDING"] = (e,g) => GetAttendees(e, EventAttendance.AttendanceStatus.Attending),
-            ["EVENT.ATTENDEES.TENTATIVE"] = (e,g) => GetAttendees(e, EventAttendance.AttendanceStatus.Tentative),
-            ["EVENT.ATTENDEES.UNAVAILABLE"] = (e,g) => GetAttendees(e, EventAttendance.AttendanceStatus.Unavailable),
+            ["EVENT.ATTENDEES.ATTENDING"] = (e, g) => GetAttendees(e, EventAttendance.AttendanceStatus.Attending),
+            ["EVENT.ATTENDEES.TENTATIVE"] = (e, g) => GetAttendees(e, EventAttendance.AttendanceStatus.Tentative),
+            ["EVENT.ATTENDEES.UNAVAILABLE"] = (e, g) => GetAttendees(e, EventAttendance.AttendanceStatus.Unavailable),
         };
         /// <summary>
         /// Fills the given template text with the macros
@@ -40,9 +38,35 @@ namespace Logbook
         /// <param name="event">The event data to fill the macros with</param>
         /// <param name="group">The group data to fill the macros with</param>
         /// <returns>The filled text</returns>
-        public static string Fill(string text, Event @event, Group group)
+        // public static string Fill(string text, Event @event, Group group)
+        // {
+        //     foreach (KeyValuePair<string, Func<Event, Group, string>> macro in macros)
+        //     {
+        //         text = text.Replace($"${{{macro.Key}}}", macro.Value(@event, group));
+        //     }
+
+        //     return text;
+        // }
+
+        public static string Fill(string text, Event @event, Models.Group group)
         {
-            foreach (KeyValuePair<string, Func<Event, Group, string>> macro in macros)
+            foreach (var parameterizedMacro in parameterizedMacros)
+            {
+                var pattern = $@"\$\{{{Regex.Escape(parameterizedMacro.Key)}\((.*?)\)\}}";
+                var regex = new Regex(pattern);
+
+                text = regex.Replace(text, match =>
+                            {
+                                // string macroName = match.Groups[1].Value;
+                                string paramString = match.Groups[1].Value;
+
+                                string[] parameters = paramString.Split(',').ToArray();
+
+                                return parameterizedMacro.Value(@event, group, parameters);
+                            });
+            }
+
+            foreach (var macro in macros)
             {
                 text = text.Replace($"${{{macro.Key}}}", macro.Value(@event, group));
             }
@@ -50,10 +74,25 @@ namespace Logbook
             return text;
         }
 
-        static string ConvertDateTime(DateTime dateTime, string timeZone, string format)
+        static string ConvertDateTime(Event @event, Models.Group group, string format, string[] parameters)
         {
-            DateTime converted = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(dateTime, "UTC", timeZone);
+            string type = GetOrDefault(parameters, 0, "start");
+            string timezone = GetOrDefault(parameters, 1, group.TimeZone);
+
+            DateTime dateTime = type.Equals("end", StringComparison.InvariantCultureIgnoreCase) ?
+                @event.EndTime : @event.StartTime;
+
+
+            DateTime converted = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(dateTime, "UTC", timezone);
+
             return converted.ToString(format);
+        }
+
+        static string GetOrDefault(string[] arr, int index, string fallback)
+        {
+            return arr.Length > index && !string.IsNullOrWhiteSpace(arr[index])
+                ? arr[index]
+                : fallback;
         }
 
         static string GetAttendees(Event @event, EventAttendance.AttendanceStatus status)
